1dcb4eed99d6db6c451a1d1a9ddda1c7b4fa96f2
John H. Emilian, III <john.emilian.iii@gmail.com>
2013-02-13 16:41:03 -0500

    Handshake through pipe.

--- forkexample.c
+++ forkexample.c
@@ -13,17 +13,24 @@
 #include <unistd.h>
 
 #include <stdlib.h> 
+#include <sys/wait.h>
 
 int g = 0;    // global
 
 //------------------------------------------------------------------------
-main()
+int main()
 //------------------------------------------------------------------------
 {
    int l = 10;    // local stack variable;
 
    int *p = malloc (sizeof (int));    // heap variable
    *p = 20;
+   int pipe_handle[2];
+
+   if (pipe(pipe_handle) < 0) {
+      printf("error in pipe()\n");
+      exit(1);
+   }
 
    pid_t pid = fork();
 
@@ -33,9 +40,11 @@ main()
       g++;
       l++;
       (*p)++;
-      printf ("in first child: pid: %d\n", getpid ());
+      printf ("in first child: pid: %d, ppid: %d\n", getpid (), getppid());
       printf ("g = %d l = %d p = %d *p=%d\n", g, l, p, (*p));
       printf ("Address: g = %ld l = %ld p = %ld\n\n\n", &g, &l, &p);
+      char *send = "hi";
+      write(pipe_handle[1], send, 64);
     }
     else if (pid < 0) {            // failed to fork
         printf ("error in fork\n");
@@ -46,33 +55,42 @@ main()
       printf ("in parent: pid: %d\n", getpid ());
       printf ("g = %d l = %d p = %d *p=%d\n", g, l, p, *p);
       printf ("Address: g = %ld l = %ld p = %ld\n\n\n", &g, &l, &p);
-      sleep(2);
 
-      pid = fork();
-      if (pid < 0) {
+      pid_t pid2 = fork();
+
+      if (pid2 < 0) {
          printf("error in fork\n");
          exit(1);
       }
-      else if (pid > 0) {
+      else if (pid2 > 0) {
          //code only executed by parent process
+         sleep(3);
+
+         char receive[64];
+         read(pipe_handle[0], receive, 64);
+         if (*receive) {
+            printf("in parent: received: %s\n\n\n", receive);
+         }
+         sleep(4);
+
          printf ("in parent: pid: %d\n", getpid ());
          printf ("g = %d l = %d p = %d *p=%d\n", g, l, p, *p);
          printf ("Address: g = %ld l = %ld p = %ld\n\n\n", &g, &l, &p);
-         int status;
-         wait(&status);
-         wait(&status);
+         //int status;
+         //wait(&status);
+         //wait(&status);
       }
       else {
          //code only executed by second child process
-         g--; l--; (*p)--; sleep(1);
-         printf("in second child: pid: %d\n", getpid());
+         g--; l--; (*p)--; sleep(5);
+         printf("in second child: pid: %d, ppid: %d\n", getpid(), getppid());
          printf("g = %d l = %d p = %d *p=%d\n", g, l, p, (*p));
          printf("Address: g = %ld l = %ld p = %ld\n\n\n", &g, &l, &p);
       }      
     }
 
     // Code executed by both parent and child.
-  
+    return 0;
 }
                 
 
