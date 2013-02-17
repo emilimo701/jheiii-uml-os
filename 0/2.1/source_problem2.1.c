e591a6449bc3359d1b75e3deb7e84ddb1abb6a8b
John H. Emilian, III <john.emilian.iii@gmail.com>
2013-02-17 14:20:36 -0500

    Sunday afternoon

--- 0/2.1/source_problem2.1.c
+++ 0/2.1/source_problem2.1.c
@@ -1,5 +1,5 @@
 //------------------------------------------------------------------------
-// signal, fork example code
+// fork example code
 // 94.308 Introduction to Operating Systems
 // Spring 2012
 // Department of Computer Science
@@ -8,10 +8,27 @@
 // Instructor: Krishnan Seetharaman
 //------------------------------------------------------------------------
 
+//------------------------------------------------------------------------
+// signal, fork example code
+// 94.308 Introduction to Operating Systems
+// Spring 2012
+// Department of Computer Science
+// University of Massachusetts Lowell
+//
+// Instructor: Krishnan Seetharaman
+//------------------------------------------------------------------------
+#define _POSIX_C_SOURCE 2
 #include <stdio.h>
-#include <stdlib.h>
-#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h> 
+
 #include <stdbool.h>
+#include <signal.h>
+
+
+#include <sys/wait.h>
+
 
 //------------------------------------------------------------------------
 int usage (char *argv[])
@@ -180,6 +197,7 @@ int processInput (int argc, char *argv[])
 
 }
 
+/*
 //------------------------------------------------------------------------
 int main(int argc, char *argv[])
 //------------------------------------------------------------------------
@@ -188,6 +206,115 @@ int main(int argc, char *argv[])
 
     processInput (argc, argv);
     return 0;
+}
+*/
+
+void sigterm_handler(int sigint) {
+   printf("in process (%d): Received signal %d. Exiting.\n", getpid(), sigint);
+   exit(0);
+}
+
+int g = 0;    // global
+
+//------------------------------------------------------------------------
+int main()
+//------------------------------------------------------------------------
+{
+   printf("\nJohn Emilian homework 0, problem 2.1\n\n");
+
+   int l = 10;    // local stack variable;
+
+   int *p = malloc (sizeof (int));    // heap variable
+   *p = 20;
+   int pipe_handle[2];
+
+   if (pipe(pipe_handle) < 0) {
+      printf("in parent (%d): error in pipe()\n", getpid());
+      exit(1);
+   }
+
+   pid_t pid = fork();
+
+   if (pid == 0) {
+      // Code only executed by first child process
+      signal(SIGTERM, sigterm_handler);
+      g++; l++; (*p)++;
+      char recv[64];
+      printf("in first child (%d): about to write message to pipe: \"%s\"\n", getpid(), "hi");
+      write(pipe_handle[1], "hi", 64);
+      printf("in first child (%d): waiting 2 seconds for ack from parent\n", getpid());
+      sleep(2); //wait for message back TODO remove sleep()
+      read(pipe_handle[0], &recv, 64);
+      printf("in first child (%d): received message through pipe: \"%s\"\n", getpid(), recv);
+      printf("in first child (%d): ppid: %d\n", getpid (), getppid());
+      printf("in first child (%d): g = %d l = %d p = %d *p=%d\n", getpid(), g, l, p, (*p));
+      printf("in first child (%d): Address: g = %ld l = %ld p = %ld\n", getpid(), &g, &l, &p);
+      while (1) {;}
+   }
+   else if (pid < 0) {
+      printf("in parent (%d): error in fork\n", getpid());
+      exit(1);
+   }
+   else {
+      // Code only executed by parent process
+      //printf ("in parent(%d): g = %d l = %d p = %d *p=%d\n", getpid(), g, l, p, *p);
+      //printf ("in parent(%d): Address: g = %ld l = %ld p = %ld\n", getpid(), &g, &l, &p);
+
+      pid_t pid2 = fork();
+
+      if (pid2 < 0) {
+         printf("in parent(%d): error in fork\n", getpid());
+         exit(1);
+      }
+      else if (pid2 > 0) {
+         //code only executed by parent process
+
+         char receive[64];
+         sleep(1); //wait for message TODO remove sleep()
+         printf("in pant (%d): waiting 1 second for message from child one\n", getpid());
+         read(pipe_handle[0], &receive, 64);
+         if (*receive) {
+            printf("in parent (%d): received: \"%s\"\n", getpid(), receive);
+            printf("in parent (%d): about to write message to pipe: \"%s\"\n", getpid(), "copy");
+            write(pipe_handle[1], "copy", 64);
+         }
+         else {
+            printf("in parent (%d): nothing in buffer from child one.\n", getpid());
+         }
+         // TODO reverse
+         //TODO handshake with child 2
+         sleep(2); //wait for child to print its ack from us TODO remove this sleep()
+
+         //printf ("in parent (%d): g = %d l = %d p = %d *p=%d\n", getpid(), g, l, p, *p);
+         //printf ("in parent (%d): Address: g = %ld l = %ld p = %ld\n", getpid(), &g, &l, &p);
+
+         sleep(3); // "7. Parent will sleep for a few seconds..."
+         printf("in parent (%d): sleeping for 3 seconds.\n", getpid());
+
+         int status, status2;
+         printf("in parent (%d): about to send signal %d to child with PID %d\n", getpid(), SIGTERM, pid);
+         kill(pid, SIGTERM);
+         waitpid(pid, &status, 0); //wait for SIGCHLD signal
+         printf("in parent (%d): Child one terminated with exit status %d\n", getpid(), status);
+         //printf("in parent (%d): about to send signal %d to child with PID %d\n", getpid(), SIGTERM, pid2);
+         //kill(pid, SIGTERM); //TODO uncomment
+         //waitpid(pid2, &status2, 0); //wait for SIGCHLD signal TODO options? , uncomment
+         //printf("in parent (%d): Child two terminated with exit status %d\n\n\n", getpid(), status2); //TODO uncomment
+         printf("in parent (%d): done\n\n", getpid());
+      }
+      else {
+         //code only executed by second child process
+         g--; l--; (*p)--; sleep(5);
+         printf("in second child (%d): ppid: %d\n", getpid(), getppid());
+         printf("in second child (%d): g = %d l = %d p = %d *p=%d\n", getpid(), g, l, p, (*p));
+         printf("in second child (%d): Address: g = %ld l = %ld p = %ld\n", getpid(), &g, &l, &p);
+         //TODO: exec
+         //while (1) {;}
+      }      
+   }
+
+   // Code executed by both parent and child.
+   exit(0);
 }
 
 
