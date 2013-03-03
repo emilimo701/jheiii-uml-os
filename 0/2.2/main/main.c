7b53c777a9d11a4190d9345a245af381de1b7843
John H. Emilian, III <john.emilian.iii@gmail.com>
2013-03-03 15:52:12 -0500

    Done with problem 2.

--- 0/2.2/main/main.c
+++ 0/2.2/main/main.c
@@ -1,5 +1,7 @@
 #define _XOPEN_SOURCE	700
-#define FILENAME_BUFSIZ	256
+#define FILENAME_BUFSIZ	256 //TODO use predefined macro
+#define PREFIX_DMSG "[DEBUG] "
+//#define DEBUG
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,36 +10,70 @@
 #include <sys/stat.h>
 #include <dirent.h>
 #include <string.h>
+#include <errno.h>
 
 off_t disk_use(const char *path) {
    struct stat file_stat;
-   stat(path, &file_stat);
+   if (stat(path, &file_stat)) {
+      //perror(errno); // question: why doesn't errno get set to a failure?
+      printf("Could not perform readdir(\"%s\").\n", path); 
+      exit(EXIT_FAILURE);
+   }
    if (S_ISDIR(file_stat.st_mode)) {
-      return (off_t) 0;
+      struct dirent *dir_entry;
+      DIR *dirptr;
+      off_t sum = 0;
+      dirptr = opendir(path);
+      while (dir_entry = readdir(dirptr)) {
+         char next_path[FILENAME_BUFSIZ];
+         strcpy(next_path, path);
+         strcat(next_path, dir_entry->d_name);
+#ifdef DEBUG
+         printf("%sEntry %s: type %d\n", PREFIX_DMSG, next_path, dir_entry->d_type);
+#endif /* DEBUG */
+         if (strcmp(dir_entry->d_name, ".") && strcmp(dir_entry->d_name, "..")) {
+            sum = sum + disk_use(next_path);
+         }
+      }
+      closedir(dirptr);
+#ifdef DEBUG
+      printf("%s%s: %li\n", PREFIX_DMSG, path, (long) sum);
+#endif /* DEBUG */
+      return sum + file_stat.st_size;
    } else {
+#ifdef DEBUG
+      printf("%s%s: %li\n", PREFIX_DMSG, path, (long) file_stat.st_size);
+#endif /* DEBUG */
       return file_stat.st_size;
    }
 }
+
 
 //------------------------------------------------------------------------
 int main(int num_args, char *arguments[])
 //------------------------------------------------------------------------
 {
    if (num_args < 2) {
-      printf("please specify path\n");
-      exit(1);
+      printf("please specify path as second argument\n");
+      exit(EXIT_FAILURE);
    }
    
    char path[FILENAME_BUFSIZ];
    strcpy(path, arguments[1]); // TODO system macro?
+#ifdef DEBUG
    struct stat file_stat;
    
    stat(path, &file_stat);
 
    off_t num_bytes = file_stat.st_size;
 
-   printf("%s: type %o, %d bytes or %d bytes\n", path, file_stat.st_mode, (int) num_bytes, (int) disk_use(path));
+   printf("%s%s: type %o, %d bytes or %li bytes\n\n", PREFIX_DMSG, path, file_stat.st_mode, (int) num_bytes, (long) disk_use(path));
+#endif /* DEBUG */
    
-   exit(0);
+#ifndef DEBUG
+   printf("%li\t%s\n", (long) disk_use(path), path);
+#endif /* ~DEBUG */
+
+   exit(EXIT_SUCCESS);
 }
 
